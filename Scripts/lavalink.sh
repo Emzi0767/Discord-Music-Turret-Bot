485e2e156c1194370f3ae3bc718eed9a2d62c4d5
Mateusz Brawański <emzi0767@gmail.com>
2018-07-20 20:34:18 +0200

    Fixed autorestart scripts.

--- Scripts/lavalink.sh
+++ Scripts/lavalink.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # This file is a part of Music Turret project.
-# 
+#
 # Copyright 2018 Emzi0767
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,7 @@ while true
 do
 	# Run lavalink
 	java -jar Lavalink.jar
-	
+
 	# Grab its exit code
 	exitcode=$?
 
