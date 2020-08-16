ba93c27c4da3af77ca41af5702c7c72f5953f23d
Mateusz Brawański <emzi0767@gmail.com>
2020-08-16 23:02:10 +0200

    Playback will stop if all users left in a VC are bots.

--- Emzi0767.MusicTurret/TurretBot.cs
+++ Emzi0767.MusicTurret/TurretBot.cs
@@ -293,7 +293,7 @@ namespace Emzi0767.MusicTurret
                 return;
 
             var usrs = chn.Users;
-            if (gmd.IsPlaying && usrs.Count() == 1 && usrs.First() == this.Discord.CurrentUser)
+            if (gmd.IsPlaying && !usrs.Any(x => !x.IsBot))
             {
                 e.Client.DebugLogger.LogMessage(LogLevel.Info, LOG_TAG, $"All users left voice in {e.Guild.Name}, pausing playback", DateTime.Now);
                 await gmd.PauseAsync();
